c5c62d3e818fa9ad8f552d7b63eaf2c26f68a224
Priyanka B <89992324+bpriya22@users.noreply.github.com>
2023-07-20 16:11:56 +0530

    commit

---
+++ python-training/hitesh_course/Level_two/lists_talks.py
@@ -0,0 +1,5 @@
+#lists are mutable
+list1=["rahul","laxman","saish"]
+list2=["umashankar","akshay","lokesh"]
+res=list1+list2
+print(res)
---
+++ python-training/hitesh_course/Level_two/string_talk.py
@@ -0,0 +1,13 @@
+# score=233
+# name="rahul"
+# print("hey",name+", my score is",score)
+# print("hey",name,", my score is",score)
+
+
+mystr="black current"
+print(mystr[2:])
+print(mystr.lower())
+
+inputs="     user    "
+print(inputs)
+print(inputs.strip())
