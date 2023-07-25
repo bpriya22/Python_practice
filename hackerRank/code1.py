4d8ba671a4be9410daf0e83f87f2f6ddcd84af22
Priyanka B <89992324+bpriya22@users.noreply.github.com>
2023-07-25 16:25:27 +0530

    code4 commit

---
+++ hackerRank/code1.py
@@ -0,0 +1,5 @@
+a=int(input("enter a number 1: "))
+b=int(input("enter a number 2: "))
+print(a+b)
+print(a-b)
+print(a*b)
---
+++ hackerRank/code4.py
@@ -0,0 +1,5 @@
+n=int(input("enter a num:"))
+while n>0 and n<150:
+    for i in range(1,n+1):
+        print(i, end="")
+    break
